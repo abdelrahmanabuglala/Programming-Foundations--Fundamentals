10c163794f6016a30ade5f90c993f8ed169390f4
abdelrahmanabuglala <77413214+abdelrahmanabuglala@users.noreply.github.com>
2021-09-01 18:49:40 +0200

    CH_4

---
+++ Chapter_4/Guessing Game.py
@@ -0,0 +1,8 @@
+favorite_food = input("what's my favorite food?\n")
+
+if favorite_food == "chicken" :
+    print("Yep! So amazing")
+else :
+    print("Yuck! That's not it")
+
+print("Thanks for playing!")
